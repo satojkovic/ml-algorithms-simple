e55436baa96d9db320a9d912875e471a8fca042d
satojkovic <satojkovic@gmail.com>
2014-04-30 21:13:30 +0900

    perceptron sample
    initial commit

---
+++ perceptron.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+
+
+class Perceptron(object):
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
+
+
+def main():
+    p = Perceptron()
+    p.train()
+    p.test()
+
+if __name__ == '__main__':
+    main()
