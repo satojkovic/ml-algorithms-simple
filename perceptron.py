5976b982b93739c121705a23bb8ea84bc72c6a0d
satojkovic <satojkovic@gmail.com>
2014-05-02 01:09:46 +0900

    perceptron algorithm

--- perceptron.py
+++ perceptron.py
@@ -8,9 +8,10 @@ class Perceptron(object):
     Perceptron Class
     """
 
-    def __init__(self, inputs, targets, T=100):
+    def __init__(self, inputs, targets, T=6, eta=0.1):
         """
         T: number of iterations
+        eta: learning rate
         N: number of training data
         m: number of inputs(exclude bias node)
         n: number of neurons
@@ -19,27 +20,53 @@ class Perceptron(object):
         targets: N x n array
         """
         self._T = T
+        self._eta = eta
         self._N = inputs.shape[0]
         self._m = inputs.shape[1]
-        self._n = targets.size[1]
-        self._w = np.random.rand(self._m+1, self._n) * 0.1 - 0.5
+        self._n = targets.shape[1]
+        self._w = np.random.rand(self._m+1, self._n) * 0.1 - 0.05
 
         bias = - np.ones((self._N, 1))
-        self._inputs = np.concatenate((inputs, bias), axis=1)
+        self._inputs = np.concatenate((bias, inputs), axis=1)
         self._targets = targets
-        self._outputs = np.zeros((self._n, 1))
+        self._outputs = np.zeros((self._N, self._n))
+
+        print 'Num of training data: %d' % self._N
+        print 'Num of input dim.: %d' % self._m
+        print 'Num of output dim.: %d' % self._n
 
     def train(self):
-        pass
+        """
+        a training phase
+        """
+        for t in xrange(self._T):
+            self._outputs = self.recall(self._inputs)
+            self._w += self._eta * np.dot(self._inputs.T, self._targets - self._outputs)
+        print '--- training phase ---'
+        print 'weights:'
+        print self._w
 
-    def recall(self):
-        pass
+    def recall(self, x):
+        """
+        activataion function
+
+        x: N x m array
+        w: m x n array
+        """
+        y = np.dot(x, self._w)
+        return np.where(y > 0, 1, 0)
 
 
 def main():
-    p = Perceptron()
+    inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    targets = np.array([[0], [1], [1], [1]])
+
+    p = Perceptron(inputs, targets)
     p.train()
-    p.recall()
+
+    print '--- recall phase ---'
+    inputs_bias = np.concatenate((-np.ones((inputs.shape[0], 1)), inputs), axis=1)
+    print p.recall(inputs_bias)
 
 if __name__ == '__main__':
     main()
