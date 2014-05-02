2fa7df96f99665b56534d3f31470b76fb0e317c6
satojkovic <satojkovic@gmail.com>
2014-05-02 23:59:02 +0900

    initial commit of multi layer perceptron

---
+++ multilayer_perceptron.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+
+import numpy as np
+
+
+class MLP(object):
+    def __init__(self, inputs, targets):
+        pass
+
+    def train(self):
+        pass
+
+    def recall(self):
+        pass
+
+
+def main():
+    mlp = MLP()
+    mlp.train()
+
+
+if __name__ == '__main__':
+    main()
