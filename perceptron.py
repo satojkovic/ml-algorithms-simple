4ce9bfe71cc7c07c828ef1fc63ed0b269a6a68c1
satojkovic <satojkovic@gmail.com>
2014-05-23 02:14:06 +0900

    Add new data

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 
 import numpy as np
+from pylab import rand
+from itertools import repeat
 
 
 class Perceptron(object):
@@ -57,6 +59,20 @@ class Perceptron(object):
         return np.where(y > 0, 1, 0)
 
 
+def gen_data(n):
+    xb = (rand(n)*2-1)/2-0.5
+    yb = (rand(n)*2-1)/2+0.5
+    inputs = [[xb[i], yb[i]] for i in xrange(len(xb))]
+    targets = [[i] for i in repeat(1, len(xb))]
+
+    xr = (rand(n)*2-1)/2+0.5
+    yr = (rand(n)*2-1)/2-0.5
+    inputs = inputs + [[xr[i], yr[i]] for i in xrange(len(xr))]
+    targets = targets + [[i] for i in repeat(-1, len(xr))]
+
+    return np.array(inputs), np.array(targets)
+
+
 def main():
     inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     targets = np.array([[0], [1], [1], [1]])
@@ -67,6 +83,15 @@ def main():
     print '--- predict phase ---'
     inputs_bias = np.concatenate((-np.ones((inputs.shape[0], 1)), inputs), axis=1)
     print p.predict(inputs_bias)
+
+    print '\n'
+    inputs2, targets2 = gen_data(10)
+    p2 = Perceptron(inputs2, targets2)
+    p2.fit()
+
+    print '\n--- predict phase ---'
+    inputs_bias2 = np.concatenate((-np.ones((inputs2.shape[0], 1)), inputs2), axis=1)
+    print p2.predict(inputs_bias2)
 
 if __name__ == '__main__':
     main()
