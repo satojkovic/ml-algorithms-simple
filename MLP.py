d4ca43ad766b4f3fcdd484cf0454d6fe15bb62a8
satojkovic <satojkovic@gmail.com>
2014-06-08 14:14:25 +0900

    XOR example

--- MLP.py
+++ MLP.py
@@ -68,35 +68,3 @@ class MLP(object):
 
     def __sigmoid_deriv(self, u):
         return (u * (1 - u))
-
-
-def main():
-    inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    targets = np.array([0, 1, 1, 0])
-
-    # initialize
-    mlp = MLP(inputs, targets, n_hidden_units=3)
-
-    print '--- initialize ---'
-    print 'Num of training data: %d' % mlp.ntd
-    print 'Num of input layer units: %d' % mlp.nin
-    print 'Num of hidden layer units: %d' % mlp.nhid
-    print 'Num of output layer units: %d' % mlp.nout
-    print 'Shape of first layer weight(v):', mlp.v.shape
-    print 'Shape of second layer weight(w):', mlp.w.shape
-
-    # training
-    mlp.fit(inputs, targets)
-    print '--- training ---'
-    print 'first layer weight: '
-    print mlp.v
-    print 'second layer weight: '
-    print mlp.w
-
-    # predict
-    print '--- predict ---'
-    for i in [[0, 0], [0, 1], [1, 0], [1, 1]]:
-        print i, mlp.predict(i)
-
-if __name__ == '__main__':
-    main()
