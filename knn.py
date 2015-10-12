83b30647454386b01f77717b9c97500c7d5decdc
satojkovic <satojkovic@gmail.com>
2015-10-12 21:57:32 +0900

    Load test data

--- knn.py
+++ knn.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
-    pass
+    trainData = np.random.randint(0, 100, (50, 2)).astype(np.float32)
+    trainLabel = np.random.randint(0, 2, (50, 1)).astype(np.float32)
+
+    # plot trainData
+    red = trainData[trainLabel.ravel() == 0]
+    blue = trainData[trainLabel.ravel() == 1]
+    plt.scatter(red[:, 0], red[:, 1], c='r')
+    plt.scatter(blue[:, 0], blue[:, 1], c='b', marker='x')
+
+    plt.show()
 
 
 if __name__ == '__main__':
