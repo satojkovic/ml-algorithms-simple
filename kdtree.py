f59cae42af038d181b9cfdf3cea59d83ef21ad9a
satojkovic <satojkovic@gmail.com>
2015-10-15 08:24:50 +0900

    Add kdtree method

--- kdtree.py
+++ kdtree.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
+
+def kdtree(pointList, depth=0):
+    if not pointList:
+        return
+
 def main():
     pointList = np.array([[2, 3],
                           [5, 4],
@@ -8,6 +15,8 @@ def main():
                           [4, 7],
                           [8, 1],
                           [7, 2]])
+
+    node = kdtree(pointList)
 
 
 if __name__ == '__main__':
