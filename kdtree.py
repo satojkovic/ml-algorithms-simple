09d5c0157be7726a8daa2180b9c25bbadf54b63f
satojkovic <satojkovic@gmail.com>
2015-10-15 22:52:10 +0900

    Create kdtree

--- kdtree.py
+++ kdtree.py
@@ -8,6 +8,20 @@ def kdtree(pointList, depth=0):
     if not pointList:
         return
 
+    # choose an axis based on the depth
+    k = len(pointList[0])
+    ax = depth % k
+
+    # choose an median along axis which to sort
+    pointList.sort(key=lambda x: x[ax])
+    median = len(pointList)/2
+
+    # create kdtree iteratively
+    return [kdtree(pointList[0:median], depth+1),   # left subtree
+            kdtree(pointList[median+1:], depth+1),  # right subtree
+            pointList[median]]
+
+
 def main():
     pointList = np.array([[2, 3],
                           [5, 4],
@@ -16,7 +30,8 @@ def main():
                           [8, 1],
                           [7, 2]])
 
-    node = kdtree(pointList)
+    tree = kdtree(pointList.tolist())
+    print tree
 
 
 if __name__ == '__main__':
