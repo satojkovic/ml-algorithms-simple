dfaf38b3e9163d556e28aca447edc89ae8386a5f
satojkovic <satojkovic@gmail.com>
2015-10-18 21:50:49 +0900

    Add a script of convolutional neural networks

---
+++ convnet.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from urllib import urlretrieve
+import cPickle as pickle
+import os
+import gzip
+
+import numpy as np
+import theano
+
+import lasagne
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+
+from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import visualize
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
