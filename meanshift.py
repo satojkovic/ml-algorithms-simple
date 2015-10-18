e2d4a698639f9461db73f247d6bfe187dd77a340
satojkovic <satojkovic@gmail.com>
2015-10-18 12:20:59 +0900

    Create a cluster and assign a cluster id to each point

--- meanshift.py
+++ meanshift.py
@@ -6,9 +6,11 @@ import numpy as np
 import multivariate_normal
 from collections import defaultdict
 from math import sqrt
+import sys
 
 
 MIN_DIST = 0.0001
+MIN_DIST_GROUP = 0.1
 
 
 def euclid_dist(p1, p2):
@@ -17,13 +19,35 @@ def euclid_dist(p1, p2):
     ]))
 
 
+def dist_to_cluster(pts, cluster):
+    min_dist = sys.float_info.max
+    min_dist_cluster_idx = None
+    for idx, center in cluster.items():
+        dist = euclid_dist(pts, center)
+        if dist < min_dist:
+            dist = min_dist
+            min_dist_cluster_idx = idx
+    return min_dist_cluster_idx, min_dist
+
+
+def nearest_cluster(pts, cluster):
+    cluster_idx = None
+    idx, dist = dist_to_cluster(pts, cluster)
+    if dist < MIN_DIST_GROUP:
+        cluster_idx = idx
+    else:
+        cluster_idx = len(cluster) + 1
+    return cluster_idx
+
+
 def mean_shift(p, points, bandwidth):
     return p
 
 
 def mean_shift_clustering(points, bandwidth):
-    cluster_num = 0
     cluster = defaultdict(list)
+    cluster_pts = defaultdict(list)
+
     for p in points:
         p_now = p
         while True:
@@ -34,10 +58,11 @@ def mean_shift_clustering(points, bandwidth):
                 break
 
         # create cluster
-        cluster[cluster_num].append(p_now)  # cluster center
-        cluster_num += 1
+        cluster_idx = nearest_cluster(p_new, cluster)
+        cluster[cluster_idx].append(p_new)  # cluster center
+        cluster_pts[cluster_idx].append(p_now)
 
-    return cluster
+    return cluster, cluster_pts
 
 
 def main():
@@ -46,7 +71,7 @@ def main():
 
     # mean shift clustering
     bandwidth = 3
-    cluster = mean_shift_clustering(X, bandwidth)
+    cluster, cluster_pts = mean_shift_clustering(X, bandwidth)
     print 'Num. of clusters:', len(cluster)
 
 
