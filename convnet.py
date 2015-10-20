2c23cc054cc7c6c24d4340d159960cd7074dbb86
satojkovic <satojkovic@gmail.com>
2015-10-21 07:53:48 +0900

    Fix typo

--- convnet.py
+++ convnet.py
@@ -58,23 +58,29 @@ def main():
     net1 = NeuralNet(
         layers=[('input', layers.InputLayer),
                 ('conv2d1', layers.Conv2DLayer),
-                ('maxppol1', layers.MaxPool2DLayer),
+                ('maxpool1', layers.MaxPool2DLayer),
                 ('conv2d2', layers.Conv2DLayer),
                 ('maxpool2', layers.MaxPool2DLayer),
                 ('dropout1', layers.DropoutLayer),
                 ('dense', layers.DenseLayer),
                 ('dropout2', layers.DropoutLayer),
                 ('output', layers.DenseLayer),
-                ],
+            ],
         # input layer
         input_shape=(None, 1, 28, 28),
-        # conv2d1 layer
+        # layer conv2d1
         conv2d1_num_filters=32,
         conv2d1_filter_size=(5, 5),
         conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
-        conv2d1_W=lasagne.init.GlorotUniform,
-        # maxpool1 layer
+        conv2d1_W=lasagne.init.GlorotUniform(),
+        # layer maxpool1
         maxpool1_pool_size=(2, 2),
+        # layer conv2d2
+        conv2d2_num_filters=32,
+        conv2d2_filter_size=(5, 5),
+        conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
+        # layer maxpool2
+        maxpool2_pool_size=(2, 2),
         # dropout1
         dropout1_p=0.5,
         # dense
@@ -87,12 +93,13 @@ def main():
         output_num_units=10,
         # optimization method params
         update=nesterov_momentum,
-        update_learning_rate=0.1,
+        update_learning_rate=0.01,
         update_momentum=0.9,
         max_epochs=10,
         verbose=1,
-        )
+    )
 
+    # Train the network
     nn = net1.fit(X_train, y_train)
 
 
