477ce88445276268945af64126e61192b06fff9b
satojkovic <satojkovic@gmail.com>
2015-11-01 15:35:03 +0900

    Load train and test data

--- linear_regression.py
+++ linear_regression.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import multivariate_normal
-import numpy as np
+from sklearn.cross_validation import train_test_split
 
 
 def main():
-    pass
+    # load sample data
+    X, X_label = multivariate_normal.load_data_with_label()
+    X_train, X_test, X_label_train, X_label_test = train_test_split(X, X_label)
 
 
 if __name__ == '__main__':
