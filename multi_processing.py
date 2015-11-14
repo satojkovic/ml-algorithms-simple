542e30aaa24666fce2d224ff0123007c8318d1f5
satojkovic <satojkovic@gmail.com>
2015-11-14 11:09:07 +0900

    Implement sample main function

--- multi_processing.py
+++ multi_processing.py
@@ -2,11 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from simple_mapreduce import SimpleMapReduce
-import multiprocessing
+
+
+def file_to_words(filename):
+    pass
+
+
+def count_words(item):
+    pass
 
 
 def main():
-    pass
+    import operator
+    import glob
+
+    input_files = glob.glob("*.rst")
+
+    mapper = SimpleMapReduce(file_to_words, count_words)
+    word_counts = mapper(input_files)
+    word_counts.sort(key=operator.itemgetter(1))
+    word_counts.reverse()
+
+    print '\nTOP 20 WORDS BY FREQUENCY\n'
+    top20 = word_counts[:20]
+    longest = max(len(word) for word, count in top20)
+    for word, count in top20:
+        print '%-*s: %5s' % (longest+1, word, count)
 
 
 if __name__ == '__main__':
