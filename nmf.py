98e33c8286936168141726897d29b1f3c87188cc
satojkovic <satojkovic@gmail.com>
2015-11-24 08:24:38 +0900

    Add sample data

--- nmf.py
+++ nmf.py
@@ -5,7 +5,21 @@ import numpy as np
 
 
 def main():
-    pass
+    # create sample data
+    R = np.array([
+        [5, 3, 0, 1],
+        [4, 0, 0, 1],
+        [1, 1, 0, 5],
+        [1, 0, 0, 4],
+        [0, 1, 5, 4],
+    ])
+
+    N = len(R)
+    M = len(R[0])
+    K = 2
+
+    P = np.random.rand(N, K)
+    Q = np.random.rand(M, K)
 
 
 if __name__ == '__main__':
