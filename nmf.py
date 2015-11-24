ab02861aa7298d5ee4921fdbc002ac25758c8ef3
satojkovic <satojkovic@gmail.com>
2015-11-25 07:58:31 +0900

    Implement my matrix factorization function

--- nmf.py
+++ nmf.py
@@ -2,6 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import itertools
+
+
+def mf(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02, threshold=0.001):
+    # Q = M x K -> Q.T = K x M
+    Q = Q.T
+
+    rows, cols = len(R), len(R[0])
+    while steps > 0:
+        # gradient descent
+        for i, j in itertools.product(xrange(rows), xrange(cols)):
+            if R[i][j] > 0:
+                eij = R[i][j] - np.dot(P[i, :], Q[:, j])
+                for k in xrange(K):
+                    P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                    Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+
+        # update
+        eR = np.dot(P, Q)
+        e = 0
+        for i, j in itertools.product(xrange(rows), xrange(cols)):
+            if R[i][j] > 0:
+                e = e + pow(R[i][j] - np.dot(P[i, :], Q[:, j]), 2)
+                for k in xrange(K):
+                    e = e + pow(R[i][j] - np.dot(P[i, :], Q[:, j]), 2) + \
+                        (beta/2) * (pow(P[i][k], 2) + pow(Q[k][j], 2))
+        if e < threshold:
+            break
+
+        # decrement steps
+        steps -= 1
+
+    return P, Q.T
 
 
 def main():
@@ -20,6 +53,11 @@ def main():
 
     P = np.random.rand(N, K)
     Q = np.random.rand(M, K)
+
+    # matrix factorization
+    nP, nQ = mf(R, P, Q, K)
+    nR = np.dot(nP, nQ.T)
+    print nR
 
 
 if __name__ == '__main__':
