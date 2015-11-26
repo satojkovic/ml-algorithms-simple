98973533711e995fbd3d6803e3b982e4187d4219
satojkovic <satojkovic@gmail.com>
2015-11-27 08:14:28 +0900

    Load sample data

--- repeated_bisection.py
+++ repeated_bisection.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import multivariate_normal
+
 
 def main():
-    pass
+    # load sample data
+    X = multivariate_normal.load_data()
 
 
 if __name__ == '__main__':
