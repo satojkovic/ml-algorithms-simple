8b206ff0a4fdedc6eec4509a142b4f2745dc6264
satojkovic <satojkovic@gmail.com>
2015-11-29 20:16:05 +0900

    Add repeated_bisection function

--- repeated_bisection.py
+++ repeated_bisection.py
@@ -4,10 +4,16 @@
 import multivariate_normal
 
 
+def repeated_bisection(X):
+    pass
+
+
 def main():
     # load sample data
     X = multivariate_normal.load_data()
 
+    # clustering a sample data
+    centers, X_labels = repeated_bisection(X)
 
 if __name__ == '__main__':
     main()
