773e818795fa5782018c37bed7d36783f7f8fc09
satojkovic <satojkovic@gmail.com>
2015-12-04 06:38:19 +0900

    Load sample data

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import multivariate_normal
+
 
 def main():
-    pass
+    # load sample data
+    X, X_labels = multivariate_normal.load_data_with_label()
+    n_labels = max(X_labels)+1
+    n_features = len(X[0])
 
 
 if __name__ == '__main__':
