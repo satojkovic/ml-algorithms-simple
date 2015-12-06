438b4a4fdc083719e6b3068bf8bdb6552bf879bb
satojkovic <satojkovic@gmail.com>
2015-12-07 08:49:43 +0900

    Add another mds sample file

---
+++ mds_sklearn_sample2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
