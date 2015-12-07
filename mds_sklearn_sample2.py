50d4905b4661b40ba77dff32f5aa16aff04de418
satojkovic <satojkovic@gmail.com>
2015-12-08 08:20:30 +0900

    Load sample data from web

--- mds_sklearn_sample2.py
+++ mds_sklearn_sample2.py
@@ -4,10 +4,15 @@
 
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 
 def main():
-    pass
+    # load sample data from web
+    # https://github.com/cheind/rmds/blob/master/examples/european_city_distances.csv
+    data = pd.read_csv('european_city_distanes.csv',
+                       header=0, sep=';')
 
 if __name__ == '__main__':
     main()
