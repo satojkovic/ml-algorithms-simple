56991ec683ea092a9bc10e3084dbbd2a7fb7d309
satojkovic <satojkovic@gmail.com>
2015-12-09 08:18:47 +0900

    Parse input data

--- mds_sklearn_sample2.py
+++ mds_sklearn_sample2.py
@@ -11,8 +11,11 @@ import pandas as pd
 def main():
     # load sample data from web
     # https://github.com/cheind/rmds/blob/master/examples/european_city_distances.csv
-    data = pd.read_csv('european_city_distanes.csv',
-                       header=0, sep=';')
+    df = pd.read_csv('european_city_distances.csv', header=None, sep=';')
+    data = df.as_matrix()
+    cities = data[:, 0]
+    dists = data[:, 1:-1]
+    dists /= np.amax(dists)
 
 if __name__ == '__main__':
     main()
