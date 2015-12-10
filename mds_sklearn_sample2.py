25ac9f71db8b42aed8df79f95c7d1b31a5f52784
satojkovic <satojkovic@gmail.com>
2015-12-11 08:12:45 +0900

    Load other sample data and plot

--- mds_sklearn_sample2.py
+++ mds_sklearn_sample2.py
@@ -5,21 +5,22 @@
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 
 def main():
-    # load sample data from web
-    # https://github.com/cheind/rmds/blob/master/examples/european_city_distances.csv
-    df = pd.read_csv('european_city_distances.csv', header=None, sep=';')
-    data = df.as_matrix()
-    cities = data[:, 0]
-    dists = data[:, 1:]
-    dists /= np.amax(dists)
+    # load sample data
+    data = np.loadtxt('distmat799.txt', delimiter=',')
+    dists = data / np.amax(data)
 
     # mds
     mds = MDS(n_components=2, dissimilarity='precomputed')
     results = mds.fit(dists)
+
+    # plot
+    plt.scatter(results.embedding_[:, 0],
+                results.embedding_[:, 1],
+                marker='o')
+    plt.show()
 
 if __name__ == '__main__':
     main()
