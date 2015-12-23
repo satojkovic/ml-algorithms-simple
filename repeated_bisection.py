e458b53bfbe646628d3def7b07ba367f764160cc
satojkovic <satojkovic@gmail.com>
2015-12-23 13:13:19 +0900

    Update methods

--- repeated_bisection.py
+++ repeated_bisection.py
@@ -6,47 +6,43 @@ from collections import defaultdict
 import numpy as np
 
 
-def choose_randomly(clusters, n_clusters):
+def choose_randomly(clusters, cluster_id):
     cluster_centers = defaultdict(np.array)
-
-    for c in range(len(clusters)):
-        idx = np.random.randint(0, len(clusters[c]))
-        cluster_centers[c] = clusters[c][idx]
-
+    idx = np.random.randint(0, len(clusters[cluster_id]), 2)
+    cluster_centers = clusters[cluster_id][idx]
     return cluster_centers
 
 
-def section(clusters, n_clusters):
-    # choose two cluster centers
-    cluster_centers = choose_randomly(clusters, n_clusters)
+def section(clusters, cluster_id, n_clusters):
+    # choose cluster centers
+    cluster_centers = choose_randomly(clusters, cluster_id)
 
     return cluster_centers
 
 
 def init_clusters(X):
-    clusters = defaultdict(list)
-
-    init_cluster_id = 0
-    for x in X:
-        clusters[init_cluster_id].append(x)
-
-    return clusters
+    clusters = defaultdict(np.array)
+    cluster_ids = np.zeros(1, dtype=np.int)
+    clusters[cluster_ids[0]] = X
+    return clusters, cluster_ids
 
 
 def repeated_bisection(X):
-    # <cluster_id, points>
-    clusters = init_clusters(X)
+    # clusters<cluster_id, points>
+    clusters, cluster_ids = init_clusters(X)
 
-    # initial two clusters
-    cluster_centers = section(clusters, 2)
+    # split an initial cluster(original data) into two clusters
+    # cluster_centers<cluster_id, coordinate_cluster_center>
+    cluster_centers = section(clusters, cluster_ids[0], 2)
 
     return cluster_centers, clusters
 
 
 def show_clusters(cluster_centers, clusters):
-    print '* n_clusters = ', len(cluster_centers)
+    print '** Result'
+    print 'n_clusters = ', len(cluster_centers)
     for i in range(len(cluster_centers)):
-        print '** cluster', i, ':', cluster_centers[i]
+        print 'cluster', i, ':', cluster_centers[i]
 
 
 def main():
