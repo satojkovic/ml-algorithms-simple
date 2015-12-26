3ca4546b8a7a659c2f420a760487e3317bafa249
satojkovic <satojkovic@gmail.com>
2015-12-26 22:06:58 +0900

    Implement bisecting step

--- repeated_bisection.py
+++ repeated_bisection.py
@@ -6,8 +6,28 @@ from collections import defaultdict
 import numpy as np
 
 
-def bisecting(clusters, cluster_centers):
-    pass
+def cos_similarity(v1, v2):
+    numerator = np.sum([e1 * e2 for e1 in v1 for e2 in v2])
+    denominator = np.sqrt(np.sum([v * v for v in v1]) *
+                          np.sum([v * v for v in v2]))
+    return numerator / denominator if denominator != 0 else 0.0
+
+
+def bisecting(clusters, cluster_id, cluster_centers):
+    sub_clusters = defaultdict(list)
+
+    # cosine similarity
+    for cidx, cluster in clusters.items():
+        max_similarity = -1.0
+        max_index = 0
+        for ccenter_idx, ccenter in enumerate(cluster_centers):
+            cos_sim = cos_similarity(cluster, ccenter)
+            if cos_sim > max_similarity:
+                max_similarity = cos_sim
+                max_index = ccenter_idx
+        sub_clusters[max_index].append(cluster)
+
+    return sub_clusters
 
 
 def choose_randomly(clusters, cluster_id):
@@ -15,21 +35,21 @@ def choose_randomly(clusters, cluster_id):
     idx = np.random.randint(0, len(clusters[cluster_id]), 2)
     cluster_centers = clusters[cluster_id][idx]
 
-    # bisecting step
-    bisecting(clusters, cluster_centers)
-
     return cluster_centers
 
 
 def section(clusters, cluster_id, n_clusters):
-    # choose cluster centers
+    # pick a cluster to split
     cluster_centers = choose_randomly(clusters, cluster_id)
 
-    return cluster_centers
+    # find 2-sub clusters using kmeans algorithm(bisecting step)
+    sub_clusters = bisecting(clusters, cluster_id, cluster_centers)
+
+    return cluster_centers, sub_clusters
 
 
 def init_clusters(X):
-    clusters = defaultdict(np.array)
+    clusters = defaultdict(list)
     cluster_ids = np.zeros(1, dtype=np.int)
     clusters[cluster_ids[0]] = X
     return clusters, cluster_ids
@@ -41,7 +61,7 @@ def repeated_bisection(X):
 
     # split an initial cluster(original data) into two clusters
     # cluster_centers<cluster_id, coordinate_cluster_center>
-    cluster_centers = section(clusters, cluster_ids[0], 2)
+    cluster_centers, clusters = section(clusters, cluster_ids[0], 2)
 
     return cluster_centers, clusters
 
@@ -50,7 +70,8 @@ def show_clusters(cluster_centers, clusters):
     print '** Result'
     print 'n_clusters = ', len(cluster_centers)
     for i in range(len(cluster_centers)):
-        print 'cluster', i, ':', cluster_centers[i]
+        print 'cluster_center', i, ':', cluster_centers[i]
+        print clusters[i]
 
 
 def main():
