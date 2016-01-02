817fdd79147db7976745264c850f780b85cc6af8
satojkovic <satojkovic@gmail.com>
2016-01-02 20:51:13 +0900

    Use scikit-learn KMeans

--- repeated_bisection.py
+++ repeated_bisection.py
@@ -4,6 +4,7 @@
 import multivariate_normal
 from collections import defaultdict
 import numpy as np
+from sklearn.cluster import KMeans
 
 
 def init_clusters(X):
@@ -73,65 +74,47 @@ def calc_cluster_centers(clusters):
     return cluster_centers
 
 
-def bisection(cluster, cluster_center):
+def bisection(cluster):
     """
     Find two sub-clusters using kmeans algorithm.
     (bisecting step)
 
     Args:
         cluster:
-        cluster_center:
     Returns:
         bisec_clusters:
-        bisec_cluster_centers:
     """
-    # choose two cluster centers
-    bisec_cluster_centers = choose_randomly(cluster)
+    km = KMeans(n_clusters=2)
+    km.fit(cluster)
+    pred_labels = km.predict(cluster)
 
-    # update cluster
     bisec_clusters = defaultdict(list)
-    cond = True
-    while cond:
-        # check if any of the centroids changed
-        cond = False
+    for label in np.unique(pred_labels):
+        bisec_clusters[label] = cluster[np.where(pred_labels == label)]
 
-        # find the closest cluster for all samples
-        for sample in cluster:
-            max_cos_sim = float("-inf")
-            max_idx = 0
-            for idx, bisec_center in enumerate(bisec_cluster_centers):
-                cos_sim = cosine_similarity(sample, bisec_center)
-                if max_cos_sim < cos_sim:
-                    max_cos_sim = cos_sim
-                    max_idx = idx
-            bisec_clusters[max_idx].append(sample)
-
-        # check if the centroid changed
-        new_centers = calc_cluster_centers(bisec_clusters)
-        for bcidx, bisec_center in enumerate(bisec_cluster_centers):
-            diff = np.linalg.norm(bisec_center - new_centers[bcidx])
-            if  diff > (1 ** -15):
-                cond = True
-
-    return bisec_clusters
+    return bisec_clusters, km.cluster_centers_
 
 
-def append_cluster(max_bisec_cluster, clusters):
-    max_id = np.max(clusters.keys())
-    for mbcidx in max_bisec_cluster:
-        clusters[max_id] = max_bisec_cluster[mbcidx]
-        max_id += 1
+def append_cluster(max_bisec_cluster, max_bisec_cluster_centers,
+                   clusters, cluster_centers):
+    """
+    Append bisectioned cluster into clusters.
+    """
+    next_id = np.max(clusters.keys())+1 if len(clusters) != 0 else 0
+    for i, id in enumerate(range(next_id, next_id+2)):
+        clusters[id] = max_bisec_cluster[i]
+        cluster_centers[id] = max_bisec_cluster_centers[i]
 
 
 def overall_similarity(bisec_clusters):
     overall_sim = 0.0
-    for cluster in bisec_clusters:
-        for cidx, sample in cluster.items():
+    for cidx, cluster in bisec_clusters.items():
+        for sample in cluster:
             overall_sim += np.dot(sample, sample)
     return overall_sim
 
 
-def repeated_bisection(X, n_clusters, ITER=10):
+def repeated_bisection(X, n_clusters, ITER=100):
     # initial cluster contains all samples
     clusters, cluster_centers = init_clusters(X)
 
@@ -145,22 +128,31 @@ def repeated_bisection(X, n_clusters, ITER=10):
         # do bisecting kmeans
         max_sim = float("-inf")
         max_bisec_clusters = None
+        max_bisec_cluster_centers = None
         for iter in range(ITER):
-            print iter
             # bisecting chosen cluster
-            bisec_clusters = bisection(cluster, cluster_center)
+            bisec_clusters, bisec_cluster_centers = bisection(cluster)
 
             # save bisec_clusters with the highest overall simlarity
             overall_sim = overall_similarity(bisec_clusters)
             if max_sim < overall_sim:
                 max_bisec_clusters = bisec_clusters
                 max_sim = overall_sim
+                max_bisec_cluster_centers = bisec_cluster_centers
 
         # append bisec_cluster to clusters
-        append_cluster(max_bisec_clusters, clusters)
-        print len(clusters)
+        append_cluster(max_bisec_clusters, max_bisec_cluster_centers,
+                       clusters, cluster_centers)
 
-    return clusters
+    return clusters, cluster_centers
+
+
+def show_results(clusters, cluster_centers, n_clusters):
+    print '** Results'
+    print 'n_clusters:', len(clusters)
+    print 'centers:'
+    for i in range(n_clusters):
+        print cluster_centers[i]
 
 
 def main():
@@ -169,11 +161,10 @@ def main():
 
     # do repeated bisection clustering
     n_clusters = 2
-    clusters = repeated_bisection(X, n_clusters)
+    clusters, cluster_centers = repeated_bisection(X, n_clusters)
 
     # show results
-    print '** Results'
-    print 'n_clusters:', len(clusters)
+    show_results(clusters, cluster_centers, n_clusters)
 
 if __name__ == '__main__':
     main()
