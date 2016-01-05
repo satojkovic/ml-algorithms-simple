da9d3b17ecd39edc95617a365d22d7e2ed6bbba8
satojkovic <satojkovic@gmail.com>
2016-01-05 10:56:37 +0900

    Add train method

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import multivariate_normal
+from sklearn.cross_validation import train_test_split
+
+
+def train(X_train, y_train):
+    pass
 
 
 def main():
@@ -10,6 +15,9 @@ def main():
     n_labels = max(X_labels)+1
     n_features = len(X[0])
 
+    # training
+    X_train, X_test, y_train, y_test = train_test_split(X, X_labels)
+    train(X_train, y_train)
 
 if __name__ == '__main__':
     main()
