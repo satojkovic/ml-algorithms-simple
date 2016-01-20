06fc36845667e2f488e84e1f33ce49d6823991fc
satojkovic <satojkovic@gmail.com>
2016-01-21 08:10:42 +0900

    Add svm.py

---
+++ svm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import multivariate_normal
+
+
+def main():
+    X, X_labels = multivariate_normal.load_data_with_label()
+
+
+if __name__ == '__main__':
+    main()
