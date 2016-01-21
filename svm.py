98f514caac488e011832307385c0435d99052ee9
satojkovic <satojkovic@gmail.com>
2016-01-22 08:39:50 +0900

    Preparing an input data

--- svm.py
+++ svm.py
@@ -7,6 +7,9 @@ import multivariate_normal
 
 def main():
     X, X_labels = multivariate_normal.load_data_with_label()
+    n_samples, n_features = X.shape
+    X = np.c_[X, np.ones(n_samples)]
+    L = np.zeros((n_samples, 1))
 
 
 if __name__ == '__main__':
