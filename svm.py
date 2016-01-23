21dfbf5869aaebe9c0afb164a01fab08479d0eb3
satojkovic <satojkovic@gmail.com>
2016-01-24 08:34:38 +0900

    Add fit and predict method

--- svm.py
+++ svm.py
@@ -3,13 +3,29 @@
 
 import numpy as np
 import multivariate_normal
+from sklearn.cross_validation import train_test_split
+
+
+def fit(X_train, y_train):
+    pass
+
+
+def predict(model, X_test, y_test):
+    pass
 
 
 def main():
+    # load sample data
     X, X_labels = multivariate_normal.load_data_with_label()
+
+    # input data
     n_samples, n_features = X.shape
     X = np.c_[X, np.ones(n_samples)]
     L = np.zeros((n_samples, 1))
+    X_train, X_test, y_train, y_test = train_test_split(X, X_labels)
+
+    # training
+    model = fit(X_train, y_train)
 
 
 if __name__ == '__main__':
