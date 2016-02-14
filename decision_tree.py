d9075305fb12f5fcc5856873fe1d4e25223efef8
satojkovic <satojkovic@gmail.com>
2016-02-15 08:10:06 +0900

    Add gini impurity method

--- decision_tree.py
+++ decision_tree.py
@@ -37,6 +37,19 @@ def uniquecounts(rows):
     return results
 
 
+def giniimpurity(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+    for k1 in counts:
+        p1 = float(counts[k1]) / total
+        for k2 in counts:
+            if k1 == k2: continue
+            p2 = float(counts[k2]) / total
+            imp += p1 * p2
+    return imp
+
+
 def main():
     my_data = np.loadtxt('decision_tree_example.txt', dtype=np.str)
     print divideset(my_data.tolist(), 2, 'yes')
