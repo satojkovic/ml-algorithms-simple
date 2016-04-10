e30a5e39d85f964e8244079ca446ca0a9fd37f7a
satojkovic <satojkovic@gmail.com>
2016-04-10 21:48:46 +0900

    Training TextCNN

--- text_cnn.py
+++ text_cnn.py
@@ -12,7 +12,7 @@ class TextCNN(object):
     """
     def __init__(
             self, sequence_length, num_classes, vocab_size,
-            embedding_size, filter_size, num_filters, l2_leg_lambda=0.0):
+            embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
 
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
@@ -34,12 +34,12 @@ class TextCNN(object):
 
         # Creates a convolutional and a maxpool layer for each filter size
         pooled_outputs = []
-        for i, filter_size in enumerate(filter_size):
+        for i, filter_size in enumerate(filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
                 # Convolution layer
                 filter_shape = [filter_size, embedding_size, 1, num_filters]
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, [num_filters]), name="b")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
                 conv = tf.nn.conv2d(
                     self.embedded_chars_expanded, W, strides=[1, 1, 1, 1],
                     padding="VALID", name="conv")
@@ -52,7 +52,7 @@ class TextCNN(object):
                 pooled_outputs.append(pooled)
 
         # Combined all the pooled outputs
-        num_filters_total = num_filters * len(filter_size)
+        num_filters_total = num_filters * len(filter_sizes)
         self.h_pool = tf.concat(3, pooled_outputs)
         self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
@@ -73,8 +73,8 @@ class TextCNN(object):
 
         # Calculate mean cross entropy loss
         with tf.name_scope("loss"):
-            losses = tf.nn.softmax_cross_entropy_with_logits(self.losses, self.input_y)
-            self.loss = tf.reduce_mean(losses) + l2_leg_lambda * l2_loss
+            losses = tf.nn.softmax_cross_entropy_with_logits(self.scores, self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
         # Calculate accuracy
         with tf.name_scope("accuracy"):
