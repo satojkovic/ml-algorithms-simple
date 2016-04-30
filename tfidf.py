e21eb8c983ff0775b4f24badca8b6f343d3d64d8
satojkovic <satojkovic@gmail.com>
2016-04-30 10:16:23 +0900

    Calculate tfidf

--- tfidf.py
+++ tfidf.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import nltk
 import os
 from bs4 import *
 import re
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
 
 PATH = 'shakes_dir'
 token_dict = {}
+stemmer = PorterStemmer()
+
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
 
 
 def to_words(raw_review):
@@ -18,6 +35,7 @@ def to_words(raw_review):
     meaningful_words = [w for w in words if not w in stops]
     return ' '.join(meaningful_words)
 
+
 def main():
     for subdir, dirs, files in os.walk(PATH):
         for file in files:
@@ -25,6 +43,9 @@ def main():
             shakes = open(file_path, 'r')
             text = shakes.read()
             token_dict[file] = to_words(text)
+
+    tfidf = TfidfVectorizer(tokenizer=tokenize)
+    tfs = tfidf.fit_transform(token_dict.values())
 
 if __name__ == '__main__':
     main()
